d2eb55fbd61fbab9ffc9b133cdb77263e5397110
Ema Zyka <emazyka@gmail.com>
2019-05-15 22:50:02 +0300

    exercises and solutions

---
+++ exercises/04-flow-control/app.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+            { id: 1, title: 'Study', status: false },
+            { id: 2, title: 'Practice', status: false },
+            { id: 3, title: 'Rest', status: false }
+        ]
+    }
+});
